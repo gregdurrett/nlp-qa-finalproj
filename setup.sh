37b04601563f4e0a4d4e4acedd583da784994b79
Greg Durrett <gregdurrett@gmail.com>
2020-11-12 22:03:07 -0600

    updating setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ wget https://s3.us-east-2.amazonaws.com/mrqa/release/v2/train/NewsQA.jsonl.gz -O
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/SQuAD.jsonl.gz -O datasets/squad_dev.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/NewsQA.jsonl.gz -O datasets/newsqa_dev.jsonl.gz
 wget http://participants-area.bioasq.org/MRQA2019/ -O datasets/bioasq.jsonl.gz
+wget http://cs.utexas.edu/~gdurrett/courses/fa2020/squad_dev_addOneSent_mrqa.json.gz -O datasets/squad_adversarial_addonesent.jsonl.gz
 
 wget http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
 unzip glove.6B.zip
